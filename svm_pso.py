9bd954131df24b90ef72dd3cf86e969655a0d5e7
mojtabamansori <13mrlien13@gmail.com>
2024-03-07 20:09:48 +0330

    roc plot

--- svm_pso.py
+++ svm_pso.py
@@ -7,6 +7,7 @@ from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 from sklearn.metrics import precision_score, recall_score, f1_score
 from pyswarm import pso
+from sklearn import metrics
 
 
 
@@ -23,7 +24,8 @@ for i in datasets:
     X_test = data_test.iloc[:, 1:].values
     y_test = data_test.iloc[:, 0].values
     temp = len(np.unique(y_test))
-    print(f'dataset {i} is running plz wait, number of class is {temp}')
+
+    print(f'dataset "" {i} "" is running plz wait, number of class is {temp}')
     X_combined = np.vstack((X_train, X_test))
     y_combined = np.concatenate((y_train, y_test))
     X_train, X_test, y_train, y_test = train_test_split(X_combined, y_combined, test_size=0.20, random_state=12)
@@ -40,13 +42,33 @@ for i in datasets:
     best_params, _ = pso(objective_function, lb, ub, swarmsize=10, maxiter=100)
     print(best_params)
     best_C, best_gamma = best_params
-    optimized_svm = SVC(kernel='rbf', C=best_C, gamma=best_gamma, class_weight='balanced')
+    optimized_svm = SVC(kernel='rbf', C=best_C, gamma=best_gamma, class_weight='balanced', probability=True)
     optimized_svm.fit(X_train, y_train)
     y_pred_optimized = optimized_svm.predict(X_test)
     final_auc = roc_auc_score(y_test, y_pred_optimized)
     precision = precision_score(y_test, y_pred_optimized)
     recall = recall_score(y_test, y_pred_optimized)
     f1 = f1_score(y_test, y_pred_optimized)
+    print(y_pred_optimized)
+    for ii in range(len(y_test)):
+        if y_test[ii]==1:
+            y_test[ii] = 0
+        if y_test[ii]==2:
+            y_test[ii] = 1
+    for ii in range(len(y_pred_optimized)):
+        if y_pred_optimized[ii]==1:
+            y_pred_optimized[ii] = 0
+        if y_pred_optimized[ii]==2:
+            y_pred_optimized[ii] = 1
+
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred_optimized)
+
+    # Create ROC curve plot
+    plt.plot(fpr, tpr)
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.title('ROC Curve')
+    plt.show()
 
     results_df = pd.DataFrame({
         'Metric': ['auc', 'Precision', 'Recall', 'F1 Score','best_c','best_gamma'],
