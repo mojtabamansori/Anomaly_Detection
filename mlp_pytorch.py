f1a9979920e4a9cb0a780df7ebb76dea96947fb9
mojtabamansori <13mrlien13@gmail.com>
2024-03-11 14:45:33 +0330

    update mpl

--- mlp_pytorch.py
+++ mlp_pytorch.py
@@ -2,38 +2,32 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score
 import torch.nn.functional as F
+from sklearn.metrics import accuracy_score
 import torch
 import torch.nn as nn
 from pyswarm import pso
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 datasets = ['Herring']
 input_size = 512
-num_epochs = 1
+num_epochs = 20  # Increased number of epochs for better convergence
 
 # Define the hyperparameter search space
-lower_bounds = [0.0001, 2, 1, 511, 255]  # Learning rate, batch size, num_epochs, hidden_size1, hidden_size2
-upper_bounds = [0.001, 12, 2, 512, 256]
+lower_bounds = [0.0001, 32, 1, 256, 150]  # Learning rate, batch size, num_epochs, hidden_size1, hidden_size2
+upper_bounds = [0.001, 64, 10, 512, 256]
 
-# Number of particles in the population
-num_particles = 10
-
-# Number of dimensions (hyperparameters)
-num_dimensions = len(lower_bounds)
-
-# Store AUC values for each run
-auc_values = []
-
-# Perform 10 runs
+num_particles = 20
+metric_values = []
 
 # Split the data into training, validation, and test sets
 for i in datasets:
     data_train = pd.read_csv(f"E:/python/UCRArchive_2018/{i}/{i}_TRAIN.tsv", sep='\t')
     data_test = pd.read_csv(f"E:/python/UCRArchive_2018/{i}/{i}_TEST.tsv", sep='\t')
 
-    X_train, X_temp, y_train, y_temp = train_test_split(data_train.iloc[:, 1:].values, data_train.iloc[:, 0].values, test_size=0.2, random_state=42)
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.3, random_state=42)
+    X_train, X_temp, y_train, y_temp = train_test_split(data_train.iloc[:, 1:].values, data_train.iloc[:, 0].values, test_size=0.2, random_state=12)
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.3, random_state=12)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -42,101 +36,108 @@ for i in datasets:
         def __init__(self, input_size, hidden_size1, hidden_size2):
             super(NeuralNet, self).__init__()
             self.fc1 = nn.Linear(input_size, hidden_size1)
-            self.relu1 = nn.ReLU()
+            self.bn1 = nn.BatchNorm1d(hidden_size1)  # Added Batch Normalization
             self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-            self.relu2 = nn.ReLU()
+            self.bn2 = nn.BatchNorm1d(hidden_size2)  # Added Batch Normalization
             self.fc3 = nn.Linear(hidden_size2, 1)
-            self.sigmoid = nn.Sigmoid()
 
         def forward(self, x):
-            out = self.fc1(x)
-            out = self.relu1(out)
-            out = self.fc2(out)
-            out = self.relu2(out)
-            out = self.fc3(out)
-            out = self.sigmoid(out)
-            return out
+            x = F.relu(self.bn1(self.fc1(x)))  # Applied Batch Normalization and ReLU activation
+            x = F.relu(self.bn2(self.fc2(x)))  # Applied Batch Normalization and ReLU activation
+            x = self.fc3(x)
+            return x
 
-    def objective(params):
-        # Unpack the hyperparameters
-        learning_rate, batch_size, num_epochs, hidden_size1, hidden_size2 = params
-
-        # Modify y_train to be in the range [0, 1]
-        y_train_modified = (y_train > 1).astype(float)
-
-        model = NeuralNet(input_size, int(hidden_size1), int(hidden_size2)).to(device)
-
-        # Define loss and optimizer
-        criterion = nn.BCEWithLogitsLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-        # Train the model
-        for epoch in range(int(num_epochs)):
-            total_loss = 0.0
-            for i2 in range(0, len(X_train) - 1, int(batch_size)):
-                x = torch.from_numpy(X_train[i2:i2 + int(batch_size), :]).to(device).float()
-                y = torch.from_numpy(y_train_modified[i2:i2 + int(batch_size)]).to(device).float()
-
-                outputs = model(x)
-                loss = criterion(outputs, y.view(-1, 1))
-                total_loss += loss.item()
+    def train_model(model, criterion, optimizer, scheduler, num_epochs, batch_size):
+        for epoch in range(num_epochs):
+            model.train()
+            running_loss = 0.0
+            for i in range(0, len(X_train), batch_size):
+                inputs = torch.from_numpy(X_train[i:i + batch_size, :]).to(device).float()
+                labels = torch.from_numpy(y_train[i:i + batch_size]).to(device).float()
 
                 optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels.view(-1, 1))
                 loss.backward()
                 optimizer.step()
 
-            average_loss = total_loss / (len(X_train) / int(batch_size))
+                running_loss += loss.item()
 
-        with torch.no_grad():
-            y_scores = []
-            y_true = []
+            epoch_loss = running_loss / (len(X_train) / batch_size)
+            scheduler.step(epoch_loss)
 
-            for i2 in range(0, len(X_val) - 1, int(batch_size)):
-                x = torch.from_numpy(X_val[i2:i2 + int(batch_size), :]).to(device).float()
-                y = torch.from_numpy(y_val[i2:i2 + int(batch_size)]).to(device).float()
-
-                outputs = model(x)
-                probabilities = torch.sigmoid(outputs)
-
-                y_scores.extend(probabilities.cpu().numpy())
-                y_true.extend(y.cpu().numpy())
-
-            auc = roc_auc_score(y_true, y_scores)
-            print(f'Run {1}: Learning Rate: {learning_rate}, Batch Size: {batch_size}, Num Epochs: {int(num_epochs)}, Hidden Size 1: {int(hidden_size1)}, Hidden Size 2: {int(hidden_size2)}')
-            print(f'AUC on the validation set: {auc}')
-            auc_values.append(auc)
-
-        return -auc  # We're using PSO to maximize AUC, so we minimize its negative value
-
-    # PSO optimization
-    start_time = time.time()
-    best_params, _ = pso(objective, lower_bounds, upper_bounds, swarmsize=num_particles, maxiter=50)
-    elapsed_time = time.time() - start_time
-
-
-
-    # Print elapsed time
-    print(f"Elapsed Time: {elapsed_time} seconds")
-
-    # After finding the best_params, use them to train the model on the full training set
-    # Evaluate the model on the test set and report the final AUC
-    with torch.no_grad():
+    def evaluate_model(model, X, y, batch_size):
+        model.eval()
         y_scores = []
         y_true = []
-        for run in range(10):
-            for i2 in range(0, len(X_test) - 1, int(best_params[1])):
-                x = torch.from_numpy(X_test[i2:i2 + int(best_params[1]), :]).to(device).float()
-                y = torch.from_numpy(y_test[i2:i2 + int(best_params[1])]).to(device).float()
-                model = NeuralNet(input_size, int(best_params[3]), int(best_params[4])).to(device)
-                outputs = model(x)
+
+        with torch.no_grad():  # Use no_grad() to disable gradient computation during evaluation
+            for i in range(0, len(X), batch_size):
+                inputs = torch.from_numpy(X[i:i + batch_size, :]).to(device).float()
+                labels = torch.from_numpy(y[i:i + batch_size]).to(device).float()
+
+                outputs = model(inputs)
                 probabilities = torch.sigmoid(outputs)
-                y_scores.extend(probabilities.cpu().numpy())
-                y_true.extend(y.cpu().numpy())
 
-            auc = roc_auc_score(y_true, y_scores)
-            print(f'Final AUC on the test set: {auc}')
-            auc_values.append(auc)
+                y_scores.extend(probabilities.cpu().detach().numpy())
+                y_true.extend(labels.cpu().detach().numpy())
 
-# Calculate and print the average AUC over 10 runs
-average_auc = np.mean(auc_values)
-print(f'Average AUC over 10 runs: {average_auc}')
+        return y_true, y_scores
+
+    def objective(params):
+        learning_rate, batch_size, num_epochs, hidden_size1, hidden_size2 = params
+
+        model = NeuralNet(input_size, int(hidden_size1), int(hidden_size2)).to(device)
+
+        criterion = nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        scheduler = ReduceLROnPlateau(optimizer, 'min')  # Learning rate scheduler
+        train_model(model, criterion, optimizer, scheduler, int(num_epochs), int(batch_size))
+        y_true_val, y_scores_val = evaluate_model(model, X_val, y_val, int(batch_size))
+
+        auc = roc_auc_score(y_true_val, y_scores_val)
+        precision = precision_score(y_true_val, (np.array(y_scores_val) > 0.5).astype(int), average='weighted',
+                                    zero_division=1)
+        recall = recall_score(y_true_val, (np.array(y_scores_val) > 0.5).astype(int), average='weighted',
+                              zero_division=1)
+
+        f1 = f1_score(y_true_val, (np.array(y_scores_val) > 0.5).astype(int), average='weighted',
+                              zero_division=1)
+        accuracy = accuracy_score(y_true_val, (np.array(y_scores_val) > 0.5).astype(int))
+
+        # You can adjust weights based on your priorities
+        metric_combination = 0.2 * auc + 0.2 * precision + 0.2 * recall + 0.2 * f1 + 0.2 * accuracy
+        metric_values.append(metric_combination)
+
+        print(f'Metric Combination: {metric_combination}, AUC: {auc}, Precision: {precision}, Recall: {recall}, F1: {f1}, Accuracy: {accuracy}')
+        return -metric_combination
+
+    start_time = time.time()
+    best_params, _ = pso(objective, lower_bounds, upper_bounds, swarmsize=num_particles, maxiter=100)
+    elapsed_time = time.time() - start_time
+
+    print(f"Elapsed Time: {elapsed_time} seconds")
+
+    model = NeuralNet(input_size, int(best_params[3]), int(best_params[4])).to(device)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=best_params[0])
+    scheduler = ReduceLROnPlateau(optimizer, 'min')
+    for _ in range(10):
+        train_model(model, criterion, optimizer, scheduler, num_epochs, int(best_params[1]))
+        y_true_test, y_scores_test = evaluate_model(model, X_test, y_test, int(best_params[1]))
+
+        threshold = 0.5  # You can adjust this threshold based on your needs
+        y_pred_binary = (np.array(y_scores_test) > threshold).astype(int)
+        auc = roc_auc_score(y_true_test, y_scores_test)
+        precision = precision_score(y_true_test, (np.array(y_scores_test) > 0.5).astype(int), average='weighted',
+                                    zero_division=1)
+        recall = recall_score(y_true_test, (np.array(y_scores_test) > 0.5).astype(int), average='weighted',
+                              zero_division=1)
+
+        f1 = f1_score(y_true_test, (np.array(y_scores_test) > 0.5).astype(int), average='weighted',
+                      zero_division=1)
+        auc_test = roc_auc_score(y_true_test, y_scores_test)
+
+        accuracy = accuracy_score(y_test, y_pred_binary)
+        print(f'Final AUC on the test set: {auc_test}, Accuracy: {accuracy},{f1},{recall},{precision}')
+        print(f'Final AUC on the test set: {auc_test}')
